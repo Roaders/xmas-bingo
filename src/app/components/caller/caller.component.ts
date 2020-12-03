415dbf00bc4d7d680bf2f59893f6aba898ca2801
unknown <10414642+Roaders@users.noreply.github.com>
2020-12-03 09:07:11 +0000

    Add randomiser draw

--- src/app/components/caller/caller.component.ts
+++ src/app/components/caller/caller.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, Observable, Subject } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { gameTiles } from 'src/app/constants';
-import { removeRandomItem } from 'src/app/helpers';
+import { pickRandomItem, removeRandomItem } from 'src/app/helpers';
 import { getSource } from 'src/app/helpers/image.helper';
+
+const drawInterval = 100;
+const minTimeout = 1000;
+const maxTimeout = 3000;
 
 @Component({
     selector: 'app-caller',
     templateUrl: './caller.component.html',
     styleUrls: ['./caller.component.scss']
 })
-export class CallerComponent {
+export class CallerComponent implements OnInit {
+
+    private _interval: number | undefined;
+    private _timeout: number | undefined;
+    private _cardsLoaded = false;
+    private _remainingCards: string[] = [];
+    private _previousCards: string[] = [];
+    private _currentCard: string | undefined;
+
+    public get drawing(): boolean{
+        return this._interval != null;
+    }
+
+    public get cardsLoaded(): boolean{
+        return this._cardsLoaded;
+    }
 
     constructor(){
         this.newGame();
+    }
+
+    public get canDraw(): boolean {
+        return !this.gameOver && this.cardsLoaded && !this.drawing;
     }
 
     public get previousCards(): string[]{
@@ -26,10 +51,9 @@ export class CallerComponent {
         return this._remainingCards.length === 0;
     }
 
-    private _remainingCards: string[] = [];
-    private _previousCards: string[] = [];
-
-    private _currentCard: string | undefined;
+    ngOnInit(): void {
+        this.loadCards();
+    }
 
     public getCardSource(value: string): string{
         return getSource(value);
@@ -40,14 +64,73 @@ export class CallerComponent {
     }
 
     public newGame(): void{
+        this.resetTimers();
         this._previousCards = [];
         this._remainingCards = Object.keys(gameTiles);
         this._currentCard = undefined;
     }
 
-    public draw(): void{
+    public draw(): void {
+        if (this._remainingCards.length < 2){
+            return this.doDraw();
+        }
+
+        this._currentCard = pickRandomItem(this._remainingCards.filter(card => card !== this._currentCard));
+
+        this._interval = setInterval(() => {
+            this._currentCard = pickRandomItem(this._remainingCards);
+        }, drawInterval) as any;
+
+        const duration = Math.floor(Math.random() * (maxTimeout - minTimeout)) + minTimeout;
+
+        this._timeout = setTimeout(() => this.doDraw(), duration) as any;
+    }
+
+    private doDraw(): void{
+        this.resetTimers();
         const item = removeRandomItem(this._remainingCards);
         this._currentCard = item;
         this._previousCards.unshift(item);
     }
+
+    private resetTimers(): void{
+        if (this._timeout != null){
+            clearTimeout(this._timeout);
+        }
+        if (this._interval != null){
+            clearInterval(this._interval);
+        }
+        this._timeout = undefined;
+        this._interval = undefined;
+
+    }
+
+    private loadCards(): void{
+        from(Object.keys(gameTiles)).pipe(
+            mergeMap(loadCard, 5)
+        )
+        .subscribe({
+            next: tile => console.log(tile),
+            complete: () => this._cardsLoaded = true,
+        });
+    }
+}
+
+function loadCard(value: string): Observable<string>{
+    const src = getSource(value);
+    const image = new Image();
+    const subject = new Subject<string>();
+
+    image.onload = () => {
+        subject.next(src);
+        subject.complete();
+    };
+
+    image.onerror = error => {
+        subject.error(error);
+    };
+
+    image.src = src;
+
+    return subject.asObservable();
 }
--- src/app/helpers/random.helper.ts
+++ src/app/helpers/random.helper.ts
@@ -1,6 +1,16 @@
 
 export function removeRandomItem<T>(values: T[]): T{
-    const index = Math.random() * values.length;
+    const index = getIndex(values);
 
     return values.splice(index, 1)[0];
+}
+
+export function pickRandomItem<T>(values: T[]): T{
+    const index = getIndex(values);
+
+    return values[index];
+}
+
+function getIndex(values: any[]): number{
+    return Math.floor(Math.random() * values.length);
 }
